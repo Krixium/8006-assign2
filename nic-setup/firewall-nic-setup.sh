c09ac229f0d4508f07d16d2c76a7b7b51019593b
Krixium <bennywang75@hotmail.com>
2019-01-28 12:29:44 -0800

    Added dynamic ip grabbing

--- nic-setup/firewall-nic-setup.sh
+++ nic-setup/firewall-nic-setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# get ip main network card
+ip_addr=$('ifconfig | grep "inet 192 " | awk -F" " "{ print $2 }"')
+
 ifconfig enp3s2 192.168.10.1 up
 echo "1" > /proc/sys/net/ipv4/ip_forward
-route add -net 192.168.0.0 netmask 255.255.255.0 gw 192.168.0.16
+route add -net 192.168.0.0 netmask 255.255.255.0 gw $ip_addr
 route add -net 192.168.10.0/24 gw 192.168.10.1
 
 iptables -F

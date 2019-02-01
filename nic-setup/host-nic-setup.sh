bf9f45cc5f6b93d9b1fc650f69f45acb5b6c504f
root <root@localhost.localdomain>
2019-01-31 16:57:06 -0800

    Added replacing the contents of /etc/resolv.conf to the host script

--- nic-setup/host-nic-setup.sh
+++ nic-setup/host-nic-setup.sh
@@ -4,4 +4,7 @@ ifconfig eno1 down
 ifconfig enp3s2 192.168.10.2 up
 route add default gw 192.168.10.1
 
+NAME_IP=142.232.76.191
+NAME_STR="nameserver $NAME_IP"
 echo "ensure /etc/resolv.conf are matching"
+grep -F "$NAME_STR" /etc/resolv.conf || echo "$NAME_STR" >> /etc/resolv.conf

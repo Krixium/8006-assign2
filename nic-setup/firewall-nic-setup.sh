ce7858de7cdb684055a446e0aea5df51cba5d34b
Krixium <bennywang75@hotmail.com>
2019-01-31 17:00:43 -0800

    Attempt at implementing port rules

--- nic-setup/firewall-nic-setup.sh
+++ nic-setup/firewall-nic-setup.sh
@@ -8,9 +8,4 @@ echo "1" > /proc/sys/net/ipv4/ip_forward
 route add -net 192.168.0.0 netmask 255.255.255.0 gw $ip_addr
 route add -net 192.168.10.0/24 gw 192.168.10.1
 
-iptables -F
-iptables -X
-iptables -A FORWARD -i eno1 -o enp3s2 -j ACCEPT
-iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
-
 echo "ensure /etc/resolv.conf are matching"
--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+tool="iptables"
+tcp="-p tcp -m tcp"
+udp="-p udp -m udp"
+eno_to_enp="-i eno1 -o enp3s2"
+
 echo "starting firewall setup ..."
 
 echo "loading user settings ..."
+
+# generic setup
+$tool -F
+$tool -X
 
 # reading in setting files to array
 declare -a icmp_in
@@ -19,38 +28,26 @@ readarray -t tcp_out < settings/tcp_out_allow.txt
 readarray -t udp_in < settings/udp_in_allow.txt
 readarray -t udp_out < settings/udp_out_allow.txt
 
-echo "allowed icmp in:"
-for type in ${icmp_in[*]}
-do
-    echo $type
-done
-
-echo "allowed icmp out:"
-for type in ${icmp_out[*]}
-do
-    echo $type
-done
-
-echo "allowed tcp in:"
 for port in ${tcp_in[*]}
 do
-    echo $port
+    $tool -A FORWARD $eno_to_enp $tcp --sport $port -j ACCEPT
+    $tool -A FORWARD $eno_to_enp $tcp --dport $port -j ACCEPT
 done
 
-echo "allowed tcp out:"
 for port in ${tcp_out[*]}
 do
-    echo $port
+    $tool -t nat -A POSTROUTING -o eno1 $tcp --sport $port -j MASQUERADE
+    $tool -t nat -A POSTROUTING -o eno1 $tcp --dport $port -j MASQUERADE
 done
 
-echo "allowed udp in:"
 for port in ${udp_in[*]}
 do
-    echo $port
+    $tool -A FORWARD $eno_to_enp $udp --sport $port -j ACCEPT
+    $tool -A FORWARD $eno_to_enp $udp --dport $port -j ACCEPT
 done
 
-echo "allowed udp out:"
 for port in ${udp_out[*]}
 do
-    echo $port
+    $tool -t nat -A POSTROUTING -o eno1 $udp --sport $port -j MASQUERADE
+    $tool -t nat -A POSTROUTING -o eno1 $udp --dport $port -j MASQUERADE
 done

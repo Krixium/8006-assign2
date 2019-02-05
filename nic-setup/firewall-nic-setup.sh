c689a2de2212a7ea1244898eee46a20b0d770611
Krixium <bennywang75@hotmail.com>
2019-02-04 16:23:49 -0800

    Cleaned up setup scripts

--- nic-setup/firewall-nic-setup.sh
+++ nic-setup/firewall-nic-setup.sh
@@ -7,5 +7,3 @@ ifconfig enp3s2 192.168.10.1 up
 echo "1" > /proc/sys/net/ipv4/ip_forward
 route add -net 192.168.0.0 netmask 255.255.255.0 gw $ip_addr
 route add -net 192.168.10.0/24 gw 192.168.10.1
-
-echo "ensure /etc/resolv.conf are matching"
--- nic-setup/host-nic-setup.sh
+++ nic-setup/host-nic-setup.sh
@@ -6,5 +6,4 @@ route add default gw 192.168.10.1
 
 NAME_IP=142.232.76.191
 NAME_STR="nameserver $NAME_IP"
-echo "ensure /etc/resolv.conf are matching"
-grep -F "$NAME_STR" /etc/resolv.conf || echo "$NAME_STR" >> /etc/resolv.conf
+echo "$NAME_STR" >> /etc/resolv.conf

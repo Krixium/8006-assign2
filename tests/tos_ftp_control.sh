4b8e36f20d03a8a6e758e2d43b95c2e84cb3f770
Data Comm <dc@bcit.ca>
2019-02-06 17:24:03 -0800

    Implemented tos tests

--- tests/tos_ftp_control.sh
+++ tests/tos_ftp_control.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 firewall_address=`cat firewall_address.txt`
 
+hping3 -c 5 -S -p 21 $firewall_address
+
--- tests/tos_ssh.sh
+++ tests/tos_ssh.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 firewall_address=`cat firewall_address.txt`
 
+hping3 -c 5 -S -p 22 $firewall_address
+
